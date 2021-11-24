12aa918adc97c0a95aa51285875cff52987d062f
Ednobre <edmilson.nobre@mercadolivre.com>
2021-11-24 10:42:49 -0300

    :tada: commit inicial do requisito 06

---
+++ src/main/java/br/com/meli/projetointegrador/exception/CouponException.java
@@ -0,0 +1,13 @@
+package br.com.meli.projetointegrador.exception;
+/**
+ * @author  Edemilson Nobre
+ * @version 5.0.0
+ * Exception personalizada para cupom
+ */
+
+public class CouponException extends RuntimeException{
+
+    public CouponException(String message) {
+        super(message);
+    }
+}
